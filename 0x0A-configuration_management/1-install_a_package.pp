ed8c8869f16f4cdc902c452e8c56d4f50ed111a9
David Njoroge <muriithid05@gmail.com>
2024-03-27 12:02:14 -0700

    new file:   0-create_a_file.pp 	new file:   1-install_a_package.pp 	new file:   2-execute_a_command.pp 	new file:   README.md

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,21 @@
+
+# Install a package using Puppet
+
+exec { 'install ruby':
+  command => '/usr/bin/apt-get install -y ruby'
+}
+
+exec { 'install puppet-lint':
+  command => '/usr/bin/gem install puppet-lint -v 2.1.0'
+}
+
+package { 'ruby':
+  ensure => 'installed',
+  before => Exec['install ruby']
+}
+
+package { 'puppet-lint':
+  ensure  => 'installed',
+  before  => Exec['install puppet-lint'],
+  require => Package['ruby']
+}
