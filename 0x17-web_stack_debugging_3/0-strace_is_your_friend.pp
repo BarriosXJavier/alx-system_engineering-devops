fe58e937a3863c8192f802d382832fd3c842ce0f
David Muriithi <muriithid05@gmail.com>
2024-05-10 09:56:32 +0000

    new file:   0-strace_is_your_friend.pp 	new file:   README.md

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fix "phpp" extension error to php in "wp-settings.php"
+
+exec{'fix-wordpress':
+	command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+	path => '/usr/local/bin/:/bin'
+}
